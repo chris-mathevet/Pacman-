8050d2da8f5ead2315d048d449ecec3714f1536a
Slyjack <62718253+slyjack999@users.noreply.github.com>
2024-01-17 11:10:10 +0100

    Co-authored-by: Axel Meunier <LeskGit@users.noreply.github.com> Co-authored-by: julian <julianmarq@users.noreply.github.com> Co-authored-by: yassfllh <yassfllh@users.noreply.github.com>

--- source/fantomes.py
+++ source/fantomes.py
@@ -20,6 +20,11 @@ test3="20;30\n##############################\n.......##.....@....##.........\n##
 plateau1=plat.Plateau(test1)
 plateau3=plat.Plateau(test3)
 
+def get_joueur(joueurs,couleur):
+    return joueurs[couleur]
+
+
+
 def creation_calque(plateau,pos,distance_max):
     """Creer un calque sur le principe de l'innondation, 
        à partir de pos en se limitant à la distance max.
@@ -106,7 +111,7 @@ def analyse_plateau_bis(plateau, pos, distance_max):
     else:
         return None
 
-def retirer_analyse(analyse,fantome):
+def retirer_analyse(analyse,fantome,pacmans_danger,analyse_bis=False):
     """A partir d'une analyse (via analyse_plateau), retire les infos inutiles au fantome donné,
         c'est à dire, les autres fantomes, le pacman de notre équipe et les vitamines
 
@@ -118,16 +123,19 @@ def retirer_analyse(analyse,fantome):
                                         et ident est l'identifiant de l'objet, du pacman ou du fantome
                                         pos est un tuple des coordonnés de l'objet
         fantome (str): couleur du fantome
+        pacmans_danger(set): ensemble des pacmans dangerex (ont l'objet glouton ou l'ont près d'eux)
+        analyse_bis(bool): booléens signifiant si c'est la deuxième analyse ou non (éviter redondence)
 
     Returns:
         dict: nouveau dictionnaire de listes, avec les informations non nécéssaire en moins
     """    
     nouvelle_analyse={"objets":[],"pacmans":[]}
-    for objets in analyse["objets"]:
-        if objets[1]!=const.VITAMINE:
-            nouvelle_analyse["objets"].append(objets)
+    if not analyse_bis:
+        for objets in analyse["objets"]:
+            if objets[1]!=const.VITAMINE:
+                nouvelle_analyse["objets"].append(objets)
     for pacmans in analyse["pacmans"]:
-        if pacmans[1]!=fantome.upper():
+        if pacmans[1]!=fantome.upper() and pacmans[1] not in pacmans_danger:
             nouvelle_analyse["pacmans"].append(pacmans)
     return nouvelle_analyse
 
@@ -161,17 +169,67 @@ def fabrique_chemin(plateau, position_depart, position_arrivee,distance_arrivee)
     return chemin
 
 def prochaine_position(plateau, position_depart, position_arrivee,distance_arrivee):
+    """renvoie la prochaine position du fantome gràce à chemin
+
+    Args:
+        plateau (plateau): un plateau de jeu
+        position_depart (tuple): un tuple de deux entiers de la forme (no_ligne, no_colonne) 
+        position_arrivee (tuple): un tuple de deux entiers de la forme (no_ligne, no_colonne) 
+        distance_arrivee (int) :  distance entre position_depart et position_arrivee (gràce à analyse) 
+
+    Returns:
+        tuple: nouvelle_pos
+    """    
     return fabrique_chemin(plateau, position_depart, position_arrivee,distance_arrivee)[-1]
 
-analyse=analyse_plateau_bis(plateau3,(3,6),10)
-print(analyse)
-print(" ")
-new_analyse=retirer_analyse(analyse,'e')
-print(new_analyse)
-chemin=fabrique_chemin(plateau3,(3,6),(5,13),10)
-print(chemin)
+#analyse=analyse_plateau_bis(plateau3,(3,6),10)
+#print(analyse)
+#print(" ")
+#new_analyse=retirer_analyse(analyse,'e')
+#print(new_analyse)
+#chemin=fabrique_chemin(plateau3,(3,6),(5,13),10)
+#print(chemin)
 
 
+
+######################################################################
+#  Ajout de Julian 
+######################################################################
+def choix_distance_pacmans(plateau, tuple_donnee,positon_fantome):
+    
+    pos= tuple_donnee[2]
+    distance = tuple_donnee[0]
+    return prochaine_position(plateau,positon_fantome, pos, distance)
+    
+def trouver_direction(pos_init, pos_arrivee):
+    direction_str = ""
+    pos_init_x,pos_init_y = pos_init
+    pos_arrivee_x,pos_arrivee_y = pos_arrivee 
+
+    diff_x = pos_init_x-pos_arrivee_x
+    diff_y = pos_init_y-pos_arrivee_y
+
+    # (0, +1) Nord
+    # (0, -1) Sud
+    # (+1, 0) Est
+    # (-1, 0) Ouest
+
+    if (diff_x, diff_y) == (0,1):
+        direction_str = "N"
+    if (diff_x, diff_y) == (0,-1):
+        direction_str = "S"
+    if (diff_x, diff_y) == (1,0):
+        direction_str = "E"
+    if (diff_x, diff_y) == (-1,0):
+        direction_str = "O"
+
+    print(diff_x,diff_y)
+    return direction_str
+
+print("par de ",(4, 7), "a", (4, 6)," donc vers le nord",trouver_direction((4, 7), (4, 6)))
+print("par de ",(4, 6), "a", (4, 7)," donc vers le Sud",trouver_direction((4, 6), (4, 7)))
+print("par de ",(5, 6), "a", (4, 6)," donc vers le Est",trouver_direction((5,6), (4, 6)))
+print("par de ",(3, 6), "a", (4, 6)," donc vers le Ouest",trouver_direction((3, 6), (4, 6)))
 
 def tri_distance_pacman(analyse):
     """_summary_
@@ -191,47 +249,137 @@ def tri_distance_pacman(analyse):
 # print(tri_distance_pacman({'objets': [(10, '!', (5, 13))], 'pacmans': [(7, 'A', (1, 2)), (1, 'C', (3, 6))]}))
 
 
-def choix_distance_pacmans(plateau, tuple_donnee,positon_fantome):
+
+ 
+####################################  PARTIE ANALYSE DES PACMANS  ####################################
+
+
+
+def pacmans_glouton(analyse,joueurs):
+    """A partir d'une analyse, renvoie un ensemble des pacmans qui ont le pouvoir glouton
+
+    Args:
+        analyse (dict): dictionnaire d'analyse
+        joueurs (dict): dictionnaire des différents joueurs
+    """    
+    pac_danger=set()
+    for _,pac,_ in analyse["pacmans"]:
+        info_joueur=get_joueur(joueurs,pac)
+        if joueur.get_duree(info_joueur,const.GLOUTON)!=0:
+            pac_danger.add(pac)
+    return 
     
-    pos= tuple_donnee[2]
-    distance = tuple_donnee[0]
-    return prochaine_position(plateau,positon_fantome, pos, distance)
-    
-def trouver_direction(plateau, pos_init, pos_arrivee):
-    direction_str = ""
-    pos_init_x,pos_init_y = pos_init
-    pos_arrivee_x,pos_arrivee_y = pos_arrivee 
-    return direction_str
-    
-def status_all_pacman(plateau):
+def analyse_pacman(analyse,plateau):
     """
     Renvoie un dictionnaire contenant en clé: l'identifiant du pacman, et en valeur : un dictionnaire avec tuple = position du pacman
-    , nb de point, et objet du pacman
+    , nb de point, et objet du pacman à partir d'une analyse du fantome
     """
     status = dict()
-    for x in range(plat.get_nb_lignes):
-        for y in range(plat.get_nb_colonnes):
-            pos = (x,y)
-            dico_case = plat.get_case(plateau,pos)
-            if case.get_pacmans(dico_case) != {}:
-                for pac in case.get_pacmans(dico_case):
-                    status[pac] = {"position_pac" : pos,"nb_points" :joueur.get_nb_points(pac),"objets" : joueur.get_objets(pac)}
+    for (_,couleur,position_pac) in analyse["pacmans"]:
+        status[couleur]=analyse_plateau_bis(plateau,position_pac,5)
     return status
+
+def glouton_proximité(analyse_pac):
+    """à partir de l'analyse pacman, renvoi un ensemble de pacmans qui ont l'objet glouton dans leur analyse
+
+    Returns:
+        set: ensemble de pacmans qui ont l'objet glouton dans leur analyse
+    """    
+    pacman_glouton = set()
+    for pacman,dico_stats in analyse_pac.items():
+        for objet in dico_stats["objets"]:
+            if const.GLOUTON in objet[1]:
+                pacman_glouton.add(pacman)
+    return pacman_glouton
+
+
+def pacmans_dangereux(glouton_prox,a_glouton):
+    """Renvoie un ensemble des pacmans dangereux, 
+    à partir des ensembles de pacmans qui on l'objet glouton et 
+    ceux qu'ils l'ont dans un rayon de 5
+
+    Args:
+        glouton_prox (set): ensemble des pacmans qui ont l'objet glouton à proximité (fonction glouton_proximité)
+        a_glouton (set): ensemble des pacmans qui ont l'objet glouton (fonction pacmans_glouton)
+
+    Returns:
+        ensemble: l'union des deux ensembles 
+    """    
+    return glouton_prox.union(a_glouton)
+    
+
+####################################  FIN PARTIE ANALYSE DES PACMANS  ####################################
+
+
                 
-def tri_proche(plateau,pos_fantome):
+def tri_proche(plateau,pos_fantome,analyse):
     """_summary_
 
     Args:
         plateau (dict): plateau de jeu
-        pos_fantome (tuple): position du fantome sur le platea
+        pos_fantome (tuple): position du fantome sur le plateau
+
+    Return une liste dans l'ordre croissant des pacmans les plus proches du fantomes
     """    
     def critere(nom):
         return dico_dist[nom]
     dico_dist = dict()
-    for pac,stats in status_all_pacman(plateau):
+    for pac,stats in analyse_pacman(analyse,plateau):
         dist = abs(stats["position_pac"][0] - pos_fantome[0]) + abs(stats["position_pac"][1] - pos_fantome[1])
         dico_dist[pac] = dist
-    dist_min = min(dico_dist, key=critere)
-    plus_proche = 0 # val random
+    dist_min = sorted(dico_dist, key=critere)
     return dist_min
         
+
+
+
+def choix(dernière_analyse,joueurs):
+    """_summary_
+
+    Returns:
+        tuple: distance par entre le fantome a la position selon le choix,ident, position selon le choix
+    """    
+    
+
+    if len(dernière_analyse["pacmans"])!=0:
+        def ratio_pac(pacman):
+            """_summary_
+
+            Args:
+                distance (int): distance par rapport au fantome
+                valeur (int): nb_points
+
+            Returns:
+                int: ratio entre distance et valeur, afin de faire un choix par la suite
+            """    
+            return (pacman[0]+joueur.get_nb_points(get_joueur(joueurs,pacman[1]))) // 2 *100
+        tri=max(dernière_analyse["pacmans"],key=ratio_pac)
+    else:
+        def ratio_objet(objet):
+            """_summary_
+
+            Args:
+                objet (_type_): _description_
+
+            Returns:
+                int: ratio entre distance et valeur, afin de faire un choix par la suite
+            """            
+            return (objet[0]+const.PROP_OBJET[objet[1]][0]) //2 *100
+        tri= max(dernière_analyse["objets"],key=ratio_objet)
+    return tri
+
+
+
+
+def IA_Fantome(joueurs,ident_fantome,plateau,position_fantome,distance=7):
+    ### Partie analyse ###
+    pacmans_danger=set()
+    analyse_fantome=analyse_plateau_bis(plateau,position_fantome,distance)
+    analyse_fantome=retirer_analyse(analyse_fantome,ident_fantome,pacmans_danger)
+    pacmans_danger=pacmans_dangereux(glouton_proximité(analyse_pacman(analyse_fantome,plateau)),pacmans_glouton(analyse_fantome,joueurs))
+    analyse_fantome=retirer_analyse(analyse_fantome,ident_fantome,pacmans_danger,True)
+    ### On détermine le choix du fantome
+    distance_choix,_,position_choix=choix()
+    prochaine_pos=prochaine_position(plateau,position_fantome,position_choix,distance_choix)
+    return trouver_direction(position_fantome,prochaine_pos)
+
--- source/pacman.py
+++ source/pacman.py
@@ -1,7 +1,4 @@
 # Fichier pour l'IA du pacman
-
-
-
 import argparse
 import random
 import client
@@ -9,7 +6,107 @@ import const
 import plateau as plat
 import case
 import joueur
+import fantomes
+test1="20;30\n##############################\n.......##..........##.........\n######.##.#####.##.##.##.#####\n######.##.#####.##.##.##.#####\n#.........#####.##.##.##.#####\n..#######.......##.##.##......\n#.#######.#####.##....##.#####\n#.##......#####.########.#####\n#.##.####.##....########....##\n#.##.####.##.##..........##.##\n#......##.##.#####.##.#####.##\n######.##.##.#####.##.#####.##\n..####.##..........##.........\n#......##.#####.##.##.##.#####\n#.####.##.#####.##.##.##.#####\n#.####....##....##.##.##....##\n#...##.##.##.##.##.##.##.##.##\n..#.##.##....##..........##...\n###....#####....###..###....##\n##############################\n5\nA;1;2\nB;1;22\nC;3;6\nD;10;21\nE;16;1\n5\na;7;5\nb;10;1\nc;10;12\nd;7;5\ne;3;6\n"
+test2="10;10\n##########\n..##..... \n# ####!###\n#.      ##\n#.#### ###\n .##..@.. \n#.##.# ###\n#.##.# ###\n#&....~ ##\n##########\n4\nA;1;1\nB;1;8\nC;8;6\nD;6;6\n4\na;8;2\nb;3;5\nc;1;9\nd;6;4\n"
+test3="20;30\n##############################\n.......##.....@....##.........\n######.##.#####.##.##.##.#####\n######.##.#####.##.##.##.#####\n#.........#####.##.##.##.#####\n#.#######....!..##.##.##......\n#.#######.#####.##....##.#####\n#.##......#####.########.#####\n#.##.####.##....########.$..##\n#.##.####.##.##..~.......##.##\n#......##.##.#####.##.#####.##\n######.##.##.#####.##.#####.##\n######.##..........##..&......\n#......##.#####.##.##.##.#####\n#.####.##.#####.##.##.##.#####\n#.####....##....##.##.##....##\n#...##.##.##.##.##.##.##.##.##\n###.##.##....##..........##...\n###.##.#####.##.###..###.##.##\n###.##.#####.##.###..###.##.##\n5\nA;1;2\nB;1;22\nC;3;6\nD;10;21\nE;16;1\n5\na;7;5\nb;10;1\nc;10;12\nd;7;5\ne;3;6\n"
+
+carte = "12;12\n.#.#..#.###.\n.#.#.@#..#..\n.#.#..#..#..\n.#.####!.#..\n............\n.####..####.\n.#..#..#..#.\n....#...&.#.\n.~..#...###.\n.####.....#.\n.#.....#..#.\n.####..####.\n5\nA;0;2\nB;1;0\nC;3;11\nD;10;5\nE;4;2\n5\na;0;2\nb;10;2\nc;10;11\nd;0;2\ne;1;0\nf;1;0\n"
+perso_map="10;10\n##########\n..##..... \n# ####!###\n#.      ##\n#.#### ###\n .##..@.. \n#.##.# ###\n#.##.#####\n#&....~ ##\n##########\n4\nA;1;1\nB;1;8\nC;8;6\nD;6;6\n4\na;8;2\nb;3;5\nc;1;9\nd;6;4\n"
 
 
+plateau1=plat.Plateau(test1)
+plateau2=plat.Plateau(test2)
+plateau3=plat.Plateau(test3)
+plateauC=plat.Plateau(carte)
+plateau_perso=plat.Plateau(perso_map)
+
+def est_un_mur(plateau):
+    """il stock tout les murs dans un ensemble 
+
+    Args:
+        plateau (dict): dico du plateau de jeu
+
+    Returns:
+        set: ensemble des coordonnées ou se situ un mur
+    """
+    murs= set()
+    lignes=plat.get_nb_lignes(plateau)
+    colonnes=plat.get_nb_colonnes(plateau)
+    for i in range(lignes):
+        for j in range(colonnes):
+            if case.est_mur(plat.get_case(plateau,(i,j))):
+                murs.add((i,j))
+    return murs
+print(est_un_mur(plateau2))
+# print(plat.Plateau(plateau2))
+
+def cul_de_sac(plateau):
+    """il mets dans un ensemble, l'ensemble des coordonnées ou se situe des culs de sacs
+
+    Args:
+        plateau (dict): dico du plateau de jeu
+
+    Returns:
+        set: ensemble des coordonnées ou se situ un cul de sac
+    """
+    pos_cul_de_sac= set()
+    lignes=plat.get_nb_lignes(plateau)
+    colonnes=plat.get_nb_colonnes(plateau)
+    for i in range (lignes):
+        for j in range(colonnes):
+            if i !=0 and j!=0 and i !=lignes-1 and j!=colonnes-1 and not len(plat.directions_possibles(plateau, (i,j), False)) != 1:
+                if (i,j) not in est_un_mur(plateau):
+                    print(len(plat.directions_possibles(plateau, (i,j), False))) 
+                    pos_cul_de_sac.add((i,j))
+            else:
+                pass
+    return pos_cul_de_sac
+
+# print(cul_de_sac(plateau1))
+print(cul_de_sac(plateau2))
+print(cul_de_sac(plateau3))
+print(cul_de_sac(plateauC))
+def test():
+    # set= {(4, 3), (4, 9), (3, 7), (9, 2), (9, 5), (1, 3), (2, 8), (6, 2), (6, 8), (4, 8), (5, 3), (9, 1), (9, 7), (9, 4), (8, 8), (1, 2), (5, 2), (3, 8), (9, 3), (8, 7), (9, 6), (1, 4), (2, 3), (2, 9), (6, 3), (6, 9)}
+    set = cul_de_sac(plateau2)
+    for co in set:
+        print("pour", co," on peut faire:",plat.directions_possibles(plateau2, co, False))
+            
+test()
+
+def intersection(plateau):
+    """il mets dans un ensemble, l'ensemble des coordonnées ou se situe des intersections de la carte
+
+    Args:
+        plateau (dict): dico du plateau de jeu
+
+    Returns:
+        set: ensemble des coordonnées ou se situ une intersection
+    """
+    les_intersections= set()
+    lignes=plat.get_nb_lignes(plateau)
+    colonnes=plat.get_nb_colonnes(plateau)
+    for i in range (lignes):
+        for j in range(colonnes):
+            if i !=0 and j!=0 and i !=lignes-1 and j!=colonnes-1 and len(plat.directions_possibles(plateau, (i,j), False)) >= 3:
+                if (i,j) not in est_un_mur(plateau):
+                    les_intersections.add((i,j))
+
+    return les_intersections
+print(intersection(plateau2))
+print(len(intersection(plateau2)))
+
+def chemin_non_cul_de_sac(plateau, cul_de_sac):
+    res = None
+    for co_cds in cul_de_sac:
+        for dir in const.DIRECTIONS:
+            plat.prochaine_intersection(plateau,co_cds,dir)
+            for co_inter in intersection(plateau):
+                res = fantomes.fabrique_chemin(plateau, co_cds,co_inter,20)
+    return res
+
+print("chemin",chemin_non_cul_de_sac(plateau_perso, cul_de_sac(plateau_perso)))
 
 
+# fabrique_chemin(plateau, position_depart, position_arrivee,distance_arrivee)
